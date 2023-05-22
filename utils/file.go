fd9a2087311073e9d4185a03707bca4e9e02c278
liulei <liu6uu@163.com>
2023-05-22 23:46:52 +0800

    添加utils.ByteSize

--- utils/file.go
+++ utils/file.go
@@ -1,6 +1,20 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	BYTE = 1 << (10 * iota)
+	KILOBYTE
+	MEGABYTE
+	GIGABYTE
+	TERABYTE
+	PETABYTE
+	EXABYTE
+)
 
 func PathExists(path string) bool {
 	_, err := os.Stat(path)
@@ -27,4 +41,36 @@ func IsFile(path string) bool {
 // Create a directory
 func MkDir(path string) error {
 	return os.MkdirAll(path, os.ModePerm)
+}
+
+func ByteSize(bytes uint64) string {
+	unit := ""
+	value := float64(bytes)
+	switch {
+	case bytes >= EXABYTE:
+		unit = "E"
+		value = value / EXABYTE
+	case bytes >= PETABYTE:
+		unit = "P"
+		value = value / PETABYTE
+	case bytes >= TERABYTE:
+		unit = "T"
+		value = value / TERABYTE
+	case bytes >= GIGABYTE:
+		unit = "G"
+		value = value / GIGABYTE
+	case bytes >= MEGABYTE:
+		unit = "M"
+		value = value / MEGABYTE
+	case bytes >= KILOBYTE:
+		unit = "K"
+		value = value / KILOBYTE
+	case bytes >= BYTE:
+		unit = "B"
+	case bytes == 0:
+		return "0"
+	}
+	result := strconv.FormatFloat(value, 'f', 1, 64)
+	result = strings.TrimSuffix(result, ".0")
+	return result + unit
 }
